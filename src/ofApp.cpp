ae9bb4b7271296ab4abf6688dd657618c1b6faa4
juiceman007 <juiceman007@users.noreply.github.com>
2018-04-04 10:45:35 -0400

    random

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,10 +1,25 @@
 #include "ofApp.h"
-
+#include <random>
 //--------------------------------------------------------------
 void ofApp::setup(){
 jewel.load("diamond.png");// image to use (must be stored in "random/bin/data" folder)
-X = {500, 1000, 30, 100, 300, 200, 750, 380 };// set of x-coordinates for jewel
-Y = {630, 400, 900,40, 200, 0, 90, 30}; //set of y-coordinates for jewel
+std::random_device rd;
+
+std::mt19937 eng1(rd());
+
+int min = 0;
+int x_max = 1200;
+int y_max = 970;
+std::uniform_int_distribution<> X_uniform(min, x_max);
+for (int j = 0; j < 20; j++){
+    X[j] = X_uniform(eng1);
+}
+std::uniform_int_distribution<> Y_uniform(min, y_max);
+for (int k = 0; k < 20; k++){
+    Y[k] = Y_uniform(eng1);
+}
+//X = {500, 1000, 30, 100, 300, 200, 750, 380 };// set of x-coordinates for jewel
+//Y = {630, 400, 900,40, 200, 0, 90, 30}; //set of y-coordinates for jewel
 }
 
 //--------------------------------------------------------------
@@ -15,8 +30,7 @@ void ofApp::update(){
 //--------------------------------------------------------------
 void ofApp::draw(){
 ofBackground(0);
-
-for(auto i = 0u; i < X.size(); i++)
+for(int i = 0; i < 20; i++)
  {
    jewel.draw(X[i], Y[i]);
  }
