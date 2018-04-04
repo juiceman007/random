b690e3f005415741f74d24686d323895757569aa
juiceman007 <juiceman007@users.noreply.github.com>
2018-04-04 11:01:24 -0400

    random

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -10,13 +10,17 @@ std::mt19937 eng1(rd());
 int min = 0;
 int x_max = 1200;
 int y_max = 970;
-std::uniform_int_distribution<> X_uniform(min, x_max);
+int x_mean = 1000;
+int y_mean = 800;
+int sd = 15;
+
+std::uniform_int_distribution<> X_uniform(x_mean, sd);
 for (int j = 0; j < 20; j++){
-    X[j] = X_uniform(eng1);
+    X.push_back(X_uniform(eng1));
 }
-std::uniform_int_distribution<> Y_uniform(min, y_max);
+std::uniform_int_distribution<> Y_uniform(y_mean, sd);
 for (int k = 0; k < 20; k++){
-    Y[k] = Y_uniform(eng1);
+    Y.push_back(Y_uniform(eng1));
 }
 //X = {500, 1000, 30, 100, 300, 200, 750, 380 };// set of x-coordinates for jewel
 //Y = {630, 400, 900,40, 200, 0, 90, 30}; //set of y-coordinates for jewel
